358af76e253a0e9022d844390514d32f69f99d5a
Shane McDonough <shanemcdman@gmail.com>
2020-02-07 00:07:12 -0800

    Initial

---
+++ main.py
@@ -0,0 +1,13 @@
+def getx0():
+    return input("Input X0> ")
+
+def getr():
+    return input("Input r> ")
+
+
+def main():
+    x = getx0()
+    r = getr()
+
+if __name__ == "__main__":
+    main()

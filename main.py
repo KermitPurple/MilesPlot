6143808addeec5c781ad6931c83f02698baa8abe
Shane McDonough <shanemcdman@gmail.com>
2020-02-07 11:11:09 -0800

    Create graph and round

--- main.py
+++ main.py
@@ -20,12 +20,28 @@ def getlist():
     x = [getx0()]
     r = getr()
     for i in range(50):
-        lst.append((i, x[-1]))
+        print(i, x[-1])
+        lst.append(x[-1])
         x.append(getnextx(x[-1], r))
     return lst
 
+def round(num):
+    roundednum = int(num)
+    if num - roundednum >= 0.5:
+        return roundednum + 1
+    return roundednum
+
 def graphlist(lst):
-    pass
+    scale = 30
+    for i in range(scale, -1, -1):
+        for lstitem in lst:
+            if round(lstitem * scale) == i:
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+         
+
 
 def main():
     lst = getlist()

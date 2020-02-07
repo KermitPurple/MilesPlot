bbe5c84b1931f14c0122c21b29bdd50403553a17
Shane McDonough <shanemcdman@gmail.com>
2020-02-07 00:26:48 -0800

    Finish math for getting values

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 def getx0():
-    return input("Input X0> ")
+    while 1:
+        try:
+            return float(input("Input X0> "))
+        except:
+            print("Invalid Input")
+
+def getnextx(xn, r):
+    return r * xn * (1-xn)
 
 def getr():
-    return input("Input r> ")
+    while 1:
+        try:
+            return float(input("Input r> "))
+        except:
+            print("Invalid Input")
 
+def getlist():
+    x = [getx0()]
+    r = getr()
+    for i in range(50):
+        print(i, x[-1])
+        x.append(getnextx(x[-1], r))
 
 def main():
-    x = getx0()
-    r = getr()
+    getlist()
 
 if __name__ == "__main__":
     main()

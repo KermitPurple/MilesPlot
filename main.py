da6dcefaa3db7eb900a31e20719696a3a760deab
Shane McDonough <shanemcdman@gmail.com>
2020-02-10 14:12:45 -0800

    Export data to csv file

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def getnextx(xn, r):
 def getr():
     while 1:
         try:
+         
             return float(input("Input r> "))
         except:
             print("Invalid Input")
 
-def getlist(x, r):
+def getlist(x, r, size):
     lst = []
-    for i in range(100):
+    for i in range(size):
         lst.append(x[-1])
         x.append(getnextx(x[-1], r))
     return lst
@@ -37,17 +38,30 @@ def graphlist(lst):
             else:
                 print(" ", end="")
         print()
-         
 
+def exportdata(lst):
+    with open("data.csv", 'a') as f:
+        for lstitem in lst:
+            f.write(str(lstitem) + ",")
+        f.write("\n")
+
+def exportheaderdata(size):
+    with open("data.csv", 'a') as f:
+        for i in range(size):
+            f.write(str(i) + ",")
+        f.write("\n")
 
 def main():
-    i = 0.001
+    i = 0.01
     r = getr()
+    lstsize = 300
+    exportheaderdata(lstsize)
     while i < 1:
-        print("\n",i)
-        lst = getlist([i],r)
-        graphlist(lst)
-        i += 0.001
+        #print("\n",i)
+        lst = getlist([i],r,lstsize)
+        #graphlist(lst)
+        exportdata(lst)
+        i += 0.01
 
 if __name__ == "__main__":
     main()
